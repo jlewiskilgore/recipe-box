48559e72ec20d07227952bf2b59177124473ce67
Lewis Kilgore <jlewis.kilgore@gmail.com>
2017-02-14 20:14:43 -0500

    Add handleSubmit function to get values from submitted form

--- src/components/AddRecipe.js
+++ src/components/AddRecipe.js
@@ -3,22 +3,35 @@ import React, { Component } from 'react';
 class AddRecipe extends Component {
 	constructor(props) {
 		super(props);
+
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleSubmit(event) {
+		event.preventDefault(); // Don't refresh the page
+		var name = this.refs.recipeName;
+		var ingredients = this.refs.recipeIngredients;
+		console.log("new recipe: ");
+		console.log(name.value + " " + ingredients.value);
+		// Clear the form's text fields
+		name.value = '';
+		ingredients.value = '';
 	}
 
 	render() {
 		return (
 			 <div id="add-recipe-form">
 			   <h2>Add Recipe</h2>
-			   <form>
+			   <form onSubmit={this.handleSubmit}>
 			     <label>
 			       Recipe Name:   
-			         <input type="text" name="recipeName" />
+			         <input type="text" ref="recipeName" />
 			     </label>
 			     <br />
 			     <br />
 			     <label>
 			       Recipe Ingredients:   
-			         <input type="text" name="recipeIngredients" />
+			         <input type="text" ref="recipeIngredients" />
 			     </label>
 			     <br />
 			     <br />
