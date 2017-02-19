307fe0803dc0141a698874f28210e230c58ea9cd
Lewis Kilgore <jlewis.kilgore@gmail.com>
2017-02-18 20:49:10 -0500

    Click list item to show/hide ingredient list

--- src/components/RecipeDetail.js
+++ src/components/RecipeDetail.js
@@ -3,15 +3,37 @@ import React, { Component } from 'react';
 class RecipeDetail extends Component {
 	constructor(props) {
 		super(props);
+
+		this.state = { showIngredients: false };
+
+		this.toggleShowIngredients = this.toggleShowIngredients.bind(this);
+	}
+
+	toggleShowIngredients() {
+		if(this.state.showIngredients == true) {
+			this.setState({ showIngredients: false });
+		}
+		else {
+			this.setState({ showIngredients: true });
+		}
 	}
 
 	render() {
-		return (
-			  <ul>
+		if(this.state.showIngredients == true) {
+			return (
+			  <ul onClick={this.toggleShowIngredients}>
 			    <h1>{this.props.recipeName}</h1>
 			    <p>{this.props.recipeIngredients}</p>
 			  </ul>
-		);
+			);
+		}
+		else {
+			return (
+			  <ul onClick={this.toggleShowIngredients}>
+			    <h1>{this.props.recipeName}</h1>
+			  </ul>
+			);
+		}
 	}
 }
 
